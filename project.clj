30f182c40b2f5da181f79878e6fba04fb10f59c6
Tim McCormack <tmccormack@brightcove.com>
2013-08-30 16:34:42 -0400

    Bump to 0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.timmc/nephila "0.1.0"
+(defproject org.timmc/nephila "0.2.0-SNAPSHOT"
   :description "Nascent project for visualizing Clojure project structure"
   :url "https://github.com/timmc/nephila"
   #_#_ :license {:name "Eclipse Public License"
