0d69b9e7f423d707c0deec990fad3999505c65a3
Tim McCormack <tmccormack@brightcove.com>
2013-09-06 13:36:11 -0400

    Bump version for development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.timmc/nephila "0.2.0"
+(defproject org.timmc/nephila "0.2.1-SNAPSHOT"
   :description "Nascent project for visualizing Clojure project structure"
   :url "https://github.com/timmc/nephila"
   :eval-in-leiningen true
