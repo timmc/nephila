78c4981911daf99b611d277a9fa937486ef55a6a
Tim McCormack <tmccormack@brightcove.com>
2013-09-09 14:05:07 -0400

    Fix realpath usage for missing files/directories.

--- doc/samples/graph-diff.sh
+++ doc/samples/graph-diff.sh
@@ -2,10 +2,14 @@
 # Show files in diff from $1 to $2 for Clojure project at $3 (output to $4)
 # Example: graph-diff.sh master my-branch server/ graph.png
 
+# Requires realpath to be on path. (Available in ubuntu as package `realpath`.)
+
 from=$1
 to=$2
 project_base=$3
-output=$(realpath $4)
+output_dir=$(dirname "$4")
+mkdir -p -- "$output_dir"
+output=$(realpath -- "$output_dir")/$(basename "$4")
 
 files=$(git diff --name-only $from...$to | sed "s,^$project_base,," | sed 's/.*/"\0"/' | paste -s -d" " -)
 
